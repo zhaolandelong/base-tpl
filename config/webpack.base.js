f78235cfe7c5c49c7effedd54d0f2e1a7d9f3b32
zhaolandelong <zhaolandelong@163.com>
2018-04-02 23:13:43 +0800

    webpack-base
    Change-Id: Iacdc2d118480748a4c58748f468a65ecb61a12a7

---
+++ config/webpack.base.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const root = path.resolve(__dirname, '../');
+
+module.exports = {
+  entry: path.resolve(root, 'src'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': path.resolve(root, 'src')
+    }
+  }
+};
