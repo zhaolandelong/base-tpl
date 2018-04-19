b7b4581071557a9be4b1143a6308dabd003a918f
zhaolandelong <zhaolandelong@163.com>
2018-04-19 12:20:45 +0800

    eslint-loader
    Change-Id: I7066c990212f87ff205213216c1369e8e4ca6855

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -10,7 +10,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
